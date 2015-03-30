26b59e8fd39f8d3ad907abc9cbf007519cbf4dd2
sok891111 <sok891111@gmail.com>
2015-03-30 15:04:03 +0900

    테스트중
    테스트

--- test.py
+++ test.py
@@ -1,20 +1,32 @@
-# XXX 
-# python encapsulation ?
-
+import copy
 
 class test:
-    def __init__(self):
-    	self.data = {};
-    def __setitem__(self,key,item):
-    	self.data[key] = item
-    def __getitem__(self, key):
-    	return self.hasattr(key) and self.data[key] or None
-    def hasattr(self,key):
-    	return self.data.get(key) and True or False
+	def __init__(self):
+		self.data = {};
+	def __setitem__(self,key,item):
+		self.data[key] = item
+	def __getitem__(self, key):
+		return self.hasattr(key) and self.data[key] or None
+	def __contains__(self, key):
+		return key in self.data
+	def __delitem__(self, key):
+		del self.data[key]
+	def copy(self):
+		return copy.deepcopy(self)
+	def get(self, key, failobj=None):
+		return self.data.get(key, failobj)
+	def hasattr(self,key):
+		return key in self.data
+
 
 t = test()
 t["a"] = {"test" : 1 }
-print t["n"]
+
+b = t.copy()
+b["a"] = 1
+del b["a"]
 print t.hasattr("a")
-
-
+print t["a"]
+print b["a"]
+print t.get("n" , "test")
+print t.get("a")
