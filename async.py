607611f009c6aff311bd1cc076017134111fcf18
sok891111 <sok891111@gmail.com>
2015-04-13 14:40:57 +0900

    비동기식 http 통신

---
+++ async.py
@@ -0,0 +1,36 @@
+import asyncore, socket, time
+
+class HTTPClient(asyncore.dispatcher):
+
+    def __init__(self, host, path):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.test_host = host
+        self.connect( (host, 80) )
+        self.buffer = 'GET %s HTTP/1.0\r\n\r\n' % path
+
+    def handle_connect(self):
+        pass
+
+    def handle_close(self):
+        self.close()
+        print self.test_host
+
+    def handle_read(self):
+        self.recv(8192)
+
+    def writable(self):
+        return (len(self.buffer) > 0)
+
+    def handle_write(self):
+        sent = self.send(self.buffer)
+        self.buffer = self.buffer[sent:]
+
+start = time.time()
+hosts = ["www.yahoo.com", "www.google.com", "www.amazon.com",
+        "www.ibm.com", "www.apple.com"]
+for host in hosts:
+	client = HTTPClient(host, '/')	
+
+asyncore.loop()
+print "Elapsed Time: %s" % (time.time() - start)
